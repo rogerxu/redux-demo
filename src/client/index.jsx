c1c76a63e7949d6038bdee948c1b53ea0b7a833d
Roger Xu <roger.xu.2007@gmail.com>
2016-11-20 16:49:32 +0800

    Add redux middlewares

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,12 +3,23 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 
+import rootReducer from './reducers';
 import App from './components/App';
-import reducer from './reducers';
 
-const store = createStore(reducer);
+
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middleware),
+);
 
 render(
   <Provider store={store}>
